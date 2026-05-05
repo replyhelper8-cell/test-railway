64b9f127ecc788cd359fc2729b279715841e4104
MENDES <remi.mendes@capgemini.com>
2026-05-05 14:57:56 +0200

    test api railway

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get("/")
+def hello():
+    return "hello railway"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
