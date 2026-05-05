9fd5856405309b2702499b8de6f4f3dca6b2848f
MENDES <remi.mendes@capgemini.com>
2026-05-05 15:36:47 +0200

    test counter

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+counter = 0
+
 @app.get("/")
 def hello():
+    global counter
+    counter += 1
     return "hello railway"
+
+@app.get("/stats")
+def stats():
+    global counter
+    return "count =" + str(counter)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
